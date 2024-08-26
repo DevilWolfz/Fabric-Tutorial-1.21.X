51d0c938b78d546be50e9564c895ef9a3c8829e8
DevilWolfz <dgkim090505@gmail.com>
2024-08-26 23:29:41 +0900

    Initial commit

---
+++ src/main/java/net/devilwolfz/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package net.devilwolfz.tutorialmod;
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
